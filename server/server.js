91b8edff5adce59bbedd29f2123ce577918853ea
Mi Kim <jaime1118@Jaimes-MacBook-Pro.local>
2019-03-22 13:07:37 -0700

    added get data endpoint in full

--- server/server.js
+++ server/server.js
@@ -1,33 +1,47 @@
 const express = require('express'); //load the express library into the file
-const mysql = require('mysql');
-const mysqlcredentials = require('./mysqlcreds.js');
+const mysql = require('mysql'); //load the mysql library
+const mysqlcredentials = require('./mysqlcreds.js'); //load the credentials from a local file for mysql
 
+//using the credentials that we loaded, establish a preliminary connection to the database
 const db = mysql.createConnection(mysqlcredentials);
 
 const server = express();
 
+//middleware
 server.use(express.static( __dirname + '/html' ));
+server.use(express.urlencoded( {extended: false} )); //have express pull body data that is urlencoded and place it into an object called "body"
 
-server.get('/api/grades', (req, res)=>{
-    res.send(`{
-      "success": true,
-      "data": [{
-        "id": 10,
-        "name": "Jaime",
-        "course": "Algebra",
-        "grade": 100
-      }, {
-        "id": 12,
-        "name": "Holmes",
-        "course": "Investigation",
-        "grade": 90
-      }, {
-        "id": 14,
-        "name": "Watson",
-        "course": "Writing",
-        "grade": 80
-      }]
-    }`)
+//make an endpoint to handle retrieving the grades of all students
+server.get('/api/grades', (req, res) => { //when this server receives request at this port ('/api/grades') call this function
+    //establish the connection to the database, and call the callback function when connection is made
+    db.connect(() => {
+        //create a query for our desired operation
+        const query = 'SELECT `id`, CONCAT(`givenname`,\" \",`surname`) AS `name`, `course`, `grade` FROM `grades`';
+        //send the query to the database, and call the given callback function once the data is retrieved or an error happens
+        db.query(query, (error, data) => {
+            //if error is null, no error occurred.
+            //create an output object to be sent back to the client
+            const output = {
+                success: false,
+            };
+            //if error is null, send the data
+            if(!error) {
+                //notify the client that we were successful
+                output.success = true;
+                //attach the data from the database to the output object
+                output.data = data;
+            } else {
+                //an error occurred, attach that error onto the output so we can see what happened
+               output.error = error;
+            }
+            //send the data back to the client
+            res.send(output);
+        })
+    })
+});
+
+server.post('/api/grades', (request, response) => {
+
 });
 
 server.listen(3001,()=>{
